4b5a7cf912297750ac2d865ccfe8426675ef2965
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-08-01 16:17:17 +0800

    m

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -29,11 +29,11 @@ DEBUG = os.getenv('DEBUG', False)
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
