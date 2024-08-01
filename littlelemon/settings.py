30620940a4eca36c1037cdce3e43742322933591
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-08-01 16:36:05 +0800

    m

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = ['deployattempt3.onrender.com']
+ALLOWED_HOSTS = []
 
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
